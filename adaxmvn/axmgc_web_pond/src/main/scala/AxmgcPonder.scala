e63727c1c6c4672eebb6be0d46dbd9485456a901
Stu Btwotwo <stub22@appstract.com>
2018-09-02 21:10:13 +0800

    Committing credits file and first sub-proj skeletons.

---
+++ adaxmvn/axmgc_web_pond/src/main/scala/AxmgcPonder.scala
@@ -0,0 +1,75 @@
+package org.appdapter.axmgc.web.pond
+
+import org.apache.jena.atlas.logging.LogCtl
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives.{path, _}
+import akka.stream.ActorMaterializer
+
+import scala.io.StdIn
+/**
+ * @author ${user.name}
+ */
+object AxmgcPonderApp {
+  val pathA = "patha"
+  val pathB = "pathb"
+  val pathJsonPreDump = "json-pre-dump"
+  val pathJsonLdMime = "json-ld-mime"
+
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+
+    LogCtl.setLog4j
+
+    import org.slf4j.LoggerFactory
+    val logger = LoggerFactory.getLogger(classOf[App])
+    logger.warn("logger warning whee")
+    launchWebServer
+  }
+  def launchWebServer : Unit = {
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+    val route =
+      path(pathA) {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+        }
+      } ~ // note tilde connects to next case
+      path(pathB) {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say goodbye to akka-http</h1>"))
+        }
+      } ~ path(pathJsonPreDump) {
+          val x = getSomeJsonLD()
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<pre>" + x + "</pre>"))
+      }  ~ path(pathJsonLdMime) {
+          val x = getSomeJsonLD()
+          // Note scala backticks, used to identify variables containing special chars
+          complete(HttpEntity(ContentTypes.`application/json`, x ))
+      }
+
+      val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+      println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+      StdIn.readLine() // let it run until user presses return
+      bindingFuture
+              .flatMap(_.unbind()) // trigger unbinding from the port
+              .onComplete(_ => system.terminate()) // and shutdown when done
+    }
+  def getSomeJsonLD() : String = {
+    val sds = new SomeDataStuff()
+    val mdl = sds.loadThatModel()
+    val mdmp = mdl.toString
+    System.out.println("Loaded: " + mdmp)
+    val jldTxt = sds.writeModelToJsonLDString_Pretty(mdl)
+    System.out.println("Formatted: " + jldTxt)
+    jldTxt
+  }
+}
