78d7eea8d91ddac668c8fa338cc8581bea0ba25d
stub22 <1082227+stub22@users.noreply.github.com>
2019-03-28 18:35:34 -0600

    More spark op tests

--- adaxmvn/axmgc_dmo_hvol/src/main/scala/axmgc/xpr/exd/TstExdLnch.scala
+++ adaxmvn/axmgc_dmo_hvol/src/main/scala/axmgc/xpr/exd/TstExdLnch.scala
@@ -1,12 +1,17 @@
 package axmgc.xpr.exd
 
 object TstExdLnch {
+	val flg_useSprkCntxt = true
 	def main(args: Array[String]): Unit = {
 		println("yowzanjobupe")
 		val workingDir = System.getProperty("user.dir");
 		println("user.dir=", workingDir)
 		val ss = new SomeSpark {}
-		ss.runBetter
+		if (flg_useSprkCntxt) {
+			ss.runInCtx
+		} else {
+			ss.runInSession
+		}
 	}
 }
 
@@ -17,17 +22,34 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
 
 trait SomeSpark {
-	val pthTl_storyTxt = "lit_txt/pg15164.txt"
+	val pthTl_folkTxt = "lit_txt/pg15164.txt"
+	val pthTl_aliceTxt = "lit_txt/pg_11_aiw_by_lc.txt"
 	val srcTxtFldr = "axmgc_dmo_hvol/src/main/resources/"
-	val rltvFilePath = srcTxtFldr + pthTl_storyTxt
-	val inFilePath = rltvFilePath
-	def runBetter : Unit = {
+	val folk_relFilePath = srcTxtFldr + pthTl_folkTxt
+	val alice_relFilePath = srcTxtFldr + pthTl_aliceTxt
+	lazy val skewed = new SimpleSkewedGroupByTest {}
+
+	def runInCtx : Unit = {
+
 		val sparkConf = new SparkConf()
-				.setAppName("Trivial App")
-				.setMaster("local[4]")  // Delete this line when submitting to a cluster
+				.setAppName("TrivialApp SparkContext")
+				.setMaster("local[4]") // Delete this line when submitting to a cluster
 
 		val sparkContext = new SparkContext(sparkConf);
 		val numbersRDD = sparkContext.parallelize(Array(2,3,2,1))
+		storyStats(sparkContext, folk_relFilePath)
+		storyStats(sparkContext, alice_relFilePath)
+		skewed.doTest(sparkContext)
+	}
+	def runInSession : Unit = {
+		// Since Spark 2.0, SparkSession is a higher level entry point for Spark, including DataFrame APIs.
+		val sprkSess = SparkSession.builder.master("local[4]").appName("TrivialApp SparkSession").getOrCreate()
+		val sprkCtx = sprkSess.sparkContext
+		storyStats(sprkCtx, folk_relFilePath)
+		sprkSess.stop()
+	}
+
+	def storyStats(sparkContext : SparkContext, inFilePath : String) : Unit = {
 		println("Defining rdd based on text file at: " + inFilePath)
 		val textRDD : RDD[String] = sparkContext.textFile(inFilePath);
 		val useRDD = textRDD
@@ -37,21 +59,15 @@ trait SomeSpark {
 		val numBs = useRDD.filter(line => line.contains("b")).count()
 		println(s"numEnts = $lineCnt, numAs = $numAs, numBs = $numBs")
 	}
-	def runStuff : Unit = {
-		val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
-		val logData = spark.read.textFile(inFilePath).cache()
-		val numAs = logData.filter(line => line.contains("a")).count()
-		val numBs = logData.filter(line => line.contains("b")).count()
-		println(s"Lines with a: $numAs, Lines with b: $numBs")
-		spark.stop()
-	}
 }
 
 /**
+  * Copied from spark test cases, and modified
+  *
   * Usage: SimpleSkewedGroupByTest [numMappers] [numKVPairs] [valSize] [numReducers] [ratio]
   */
-object SimpleSkewedGroupByTest {
-	def main(args: Array[String]) {
+trait SimpleSkewedGroupByTest {
+	def doMain(args: Array[String]) {
 		val spark = SparkSession
 				.builder
 				.appName("SimpleSkewedGroupByTest")
@@ -63,7 +79,15 @@ object SimpleSkewedGroupByTest {
 		val numReducers = if (args.length > 3) args(3).toInt else numMappers
 		val ratio = if (args.length > 4) args(4).toInt else 5.0
 
-		val pairs1 = spark.sparkContext.parallelize(0 until numMappers, numMappers).flatMap { p =>
+		doTest(spark.sparkContext)
+
+		spark.stop()
+	}
+
+	def doTest(sprkCtx : SparkContext, numMappers : Int = 2, numKVPairs : Int = 1000,
+			   valSize : Int = 1000, numReducers : Int = 2, ratio : Float = 5.0F) : Unit = {
+		println("Starting SimpleSkewedGroupByTest");
+		val pairs1 = sprkCtx.parallelize(0 until numMappers, numMappers).flatMap { p =>
 			val ranGen = new Random
 			val result = new Array[(Int, Array[Byte])](numKVPairs)
 			for (i <- 0 until numKVPairs) {
@@ -81,11 +105,12 @@ object SimpleSkewedGroupByTest {
 			}
 			result
 		}.cache
+		println("SimpleSkewedGroupByTest:  Pairs have been told to cache, starting count.")
 		// Enforce that everything has been calculated and in cache
-		pairs1.count
-
-		println(s"RESULT: ${pairs1.groupByKey(numReducers).count}")
-
-		spark.stop()
+		val cnt = pairs1.count
+		println(s"SimpleSkewedGroupByTest: Count = $cnt")
+		val grpd: RDD[(Int, Iterable[Array[Byte]])] = pairs1.groupByKey(numReducers)
+		val grpdCnt : Long = grpd.count()
+		println(s"SimpleSkewedGroupByTest grouped count: $grpdCnt")
 	}
 }
