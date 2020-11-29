9fe6322af20a04ea2dcdae8d38700bfb8b196dc4
stub22 <1082227+stub22@users.noreply.github.com>
2020-11-29 06:37:28 -0800

    Oracle NoSQL/Berkeley-java DB java launches server, connects client, stores+gets a value. using maven deps only

--- adaxmvn/axmgc_dmo_fibo/src/main/scala/axmgc/dmo/fin/ontdmp/borkl/BorklGo.scala
+++ adaxmvn/axmgc_dmo_fibo/src/main/scala/axmgc/dmo/fin/ontdmp/borkl/BorklGo.scala
@@ -1,5 +1,7 @@
 package axmgc.dmo.fin.ontdmp.borkl
 
+import oracle.kv.{KVStoreConfig, KVStoreFactory, Version}
+
 private trait BorklGo
 
 trait ServONJ {
@@ -9,8 +11,57 @@ trait ServONJ {
 }
 
 trait CliONJ {
+	def testCliConn() : Unit = {
+		val dfltStoreName = "kvstore"
+		val otherStoreName = "kvs_borkl_29"
+		val storeName = dfltStoreName
+		val hostName = "localhost"
+		val hostPort = "5000"
+		println ("Making kvsConf")
+		val kvsConf = new KVStoreConfig(storeName, hostName + ":" + hostPort)
+		println ("Connecting kvsCli")
+		val kvsCli =  KVStoreFactory.getStore(kvsConf);
+		println ("Got kvsCli:" + kvsCli)
 
+		import oracle.kv.Key
+		import oracle.kv.Value
+		import oracle.kv.ValueVersion
+		val keyString = "Hello"
+		val valueString = "Big Data World!"
+
+		val goodKey: Key = Key.createKey(keyString)
+		val goodVal: Value = Value.createValue(valueString.getBytes)
+
+		val putVV: Version = kvsCli.put(goodKey, goodVal)
+		println("putVV: " + putVV)
+
+		val gotVV: ValueVersion = kvsCli.get(goodKey)
+
+		println(keyString + " " + new String(gotVV.getValue.getValue))
+		kvsCli.close()
+	}
 }
+
+/*
+Opened existing kvlite store with config:
+-root onor_001 -store kvstore -host sqdgyrct -port 5000 -secure-config disable -restore-from-snapshot null -admin-web-port 8693
+goServGo returned, now we will sleep 5S and then try to connect
+Back from sleep
+Making kvsConf
+Connecting kvsCli
+Exception in thread "main" java.lang.IllegalArgumentException: Specified store name, kvs_borkl_29, does not match store name at specified host/port, kvstore
+	at oracle.kv.impl.api.RequestDispatcherImpl.<init>(RequestDispatcherImpl.java:445)
+	at oracle.kv.impl.api.RequestDispatcherImpl.<init>(RequestDispatcherImpl.java:371)
+	at oracle.kv.impl.api.RequestDispatcherImpl.createForClient(RequestDispatcherImpl.java:344)
+	at oracle.kv.KVStoreFactory.getStoreInternal(KVStoreFactory.java:253)
+	at oracle.kv.KVStoreFactory.getStore(KVStoreFactory.java:135)
+	at oracle.kv.KVStoreFactory.getStore(KVStoreFactory.java:72)
+	at axmgc.dmo.fin.ontdmp.borkl.CliONJ.testCliConn(BorklGo.scala:22)
+	at axmgc.dmo.fin.ontdmp.borkl.CliONJ.testCliConn$(BorklGo.scala:14)
+	at axmgc.dmo.fin.ontdmp.borkl.RunTstBorkl$$anon$2.testCliConn(RunTstBorkl.scala:15)
+	at axmgc.dmo.fin.ontdmp.borkl.RunTstBorkl$.main(RunTstBorkl.scala:16)
+	at axmgc.dmo.fin.ontdmp.borkl.RunTstBorkl.main(RunTstBorkl.scala)
+ */
 
 trait BerkConf {
 
--- adaxmvn/axmgc_dmo_fibo/src/main/scala/axmgc/dmo/fin/ontdmp/borkl/RunTstBorkl.scala
+++ adaxmvn/axmgc_dmo_fibo/src/main/scala/axmgc/dmo/fin/ontdmp/borkl/RunTstBorkl.scala
@@ -9,5 +9,13 @@ object RunTstBorkl {
 		val goodArgs = List("-root", "onor_001", "-secure-config", "disable", "-admin-web-port", "8693").toArray
 
 		bg.goServGo(goodArgs)
+		println("goServGo returned, now we will sleep 5S and then try to connect")
+		Thread.sleep(5000L)
+		println("Back from sleep")
+		val cc = new CliONJ {}
+		cc.testCliConn()
+		println("Finished testCliConn")
+		println("End of Main")
 	}
+
 }
