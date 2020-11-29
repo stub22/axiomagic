523ed6a6fedc1f261359c7ef096416ea4b8948a0
stub22 <1082227+stub22@users.noreply.github.com>
2020-11-29 11:40:44 -0800

    Using borkl TableAPI and some command line tools.

--- adaxmvn/axmgc_dmo_fibo/src/main/scala/axmgc/dmo/fin/ontdmp/borkl/BorklGo.scala
+++ adaxmvn/axmgc_dmo_fibo/src/main/scala/axmgc/dmo/fin/ontdmp/borkl/BorklGo.scala
@@ -1,6 +1,7 @@
 package axmgc.dmo.fin.ontdmp.borkl
 
-import oracle.kv.{KVStoreConfig, KVStoreFactory}
+import oracle.kv.table.{Row, Table}
+import oracle.kv.{KVStore, KVStoreConfig, KVStoreFactory, StatementResult, Version}
 
 private trait BorklGo
 
@@ -21,7 +22,7 @@ trait CliONJ {
 		println ("Making kvsConf")
 		val kvsConf = new KVStoreConfig(storeName, hostName + ":" + hostPort)
 		println ("Connecting kvsCli")
-		val kvsCli =  KVStoreFactory.getStore(kvsConf);
+		val kvsCli: KVStore =  KVStoreFactory.getStore(kvsConf);
 		println ("Got kvsCli:" + kvsCli)
 
 		import oracle.kv.{Key, Value, Version, ValueVersion}
@@ -37,9 +38,68 @@ trait CliONJ {
 		val gotVV: ValueVersion = kvsCli.get(goodKey)
 
 		println(keyString + " " + new String(gotVV.getValue.getValue))
+
+		val mkTb = new MkKVTables {}
+		mkTb.tstTableStorage(kvsCli)
 		kvsCli.close()
 	}
 }
+
+trait MkKVTables {
+	val tblMkCmd =  "CREATE TABLE IF NOT EXISTS simpleUsers  " +
+				"(firstName STRING, " +
+				" lastName STRING, " +
+				" userID INTEGER, " +
+				" PRIMARY KEY (userID))"
+
+	val tblDropCmd ="DROP TABLE IF EXISTS simpleUsers"
+
+	def tstTableStorage(kvCli : KVStore) : Unit = {
+		val drpRslt: StatementResult = kvCli.executeSync(tblDropCmd)
+
+		val creRslt: StatementResult = kvCli.executeSync(tblMkCmd)
+
+		val kvTblApi = kvCli.getTableAPI
+
+		val tblCli: Table =  kvTblApi.getTable("simpleUsers")
+
+		/* Insert row */
+		val oneRow: Row = tblCli.createRow
+		oneRow.put("userID", 1)
+		oneRow.put("firstName", "Alex")
+		oneRow.put("lastName", "Robertson")
+		val putRslt: Version = kvTblApi.put(oneRow, null, null)
+
+
+	}
+}
+/*
+This client works for Stu using Version 20.2.21, but we cannot find the sql.jar in earlier versions.
+Those do contain kvclient.jar, but we have not figured out how to launch its kv admin interface
+as a console client, so we are sticking with this SQL jobber for now.
+
+https://docs.oracle.com/en/database/other-databases/nosql-database/18.1/sqlfornosql/introduction-sql-shell.html
+
+SQLDeveloper also works for browsing the schema.
+
+It looks like kvclient.jar probably works fine on the classpath for compiling + running example code.
+(We have that capability already on our maven classpath).
+
+CONSOLE_DIR      /c/_root/_japp/orcl_kvcli_20221
+CONSOLE_PROMPT   java -jar lib/sql.jar
+sql-> help
+sql-> connect -host localhost -port 5000 -name kvstore
+Connected to kvstore at localhost:5000.
+sql-> show tables
+
+sql-> select * from simpleUsers WHERE firstName = 'Alex';
+{"firstName":"Alex","lastName":"Robertson","userID":1}
+
+
+More API info:
+https://docs.oracle.com/en/database/other-databases/nosql-database/18.1/java-driver-table/developing.html
+
+ */
 
 /*
 Opened existing kvlite store with config:
