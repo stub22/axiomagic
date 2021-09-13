5863c8c9c34c4cea08818350b6771767625d5a0e
stub22 <1082227+stub22@users.noreply.github.com>
2021-09-12 20:52:42 -0700

    added new demo module "bgdt" for big data demos including parquet file io

---
+++ adaxmvn/axmgc_dmo_bgdt/src/main/scala/axmgc/xpr/io/prqt/TestPrqtIoEasily.scala
@@ -0,0 +1,45 @@
+package axmgc.dmo.xpr.io.prqt
+
+import org.slf4j.{Logger, LoggerFactory}
+import axmgc.web.lnch.FallbackLog4J
+
+import java.util.{Map => JMap, Iterator => JIterator}
+
+import scala.collection.immutable.{Seq, Map => SMap}
+import scala.collection.mutable.{ListBuffer => SMListBuf}
+
+object TestPrqtIoEasily {
+
+	val flg_setupFallbackLog4J = false // Set to false if log4j.properties is expected, e.g. from Jena.
+	val myFallbackLog4JLevel = org.apache.log4j.Level.INFO
+	lazy val myFLog4J = new FallbackLog4J {}
+
+	private val rsrcPth_lmlExpWebJson = "/gdat/lean_mathlib/lml_exweb_20210521_sz196MB.json"
+	private val rsrcPth_lmlExpStrctJson = "/gdat/lean_mathlib/lml_exstruct_20210529_sz91MB.json"
+
+	def main(args: Array[String]) {
+		if (flg_setupFallbackLog4J) {
+			myFLog4J.setupFallbackLogging(myFallbackLog4JLevel)
+		}
+
+		val prqtSrcScanner = new PrqtSourceScanner(rsrcPth_lmlExpWebJson, rsrcPth_lmlExpStrctJson)
+
+		prqtSrcScanner.doScan()
+	}
+}
+
+class PrqtSourceScanner(rsrcPth_lmlExpWebJson : String, rsrcPth_lmlExpStrctJson : String) {
+	val myS4JLogger: Logger = LoggerFactory.getLogger(this.getClass)
+
+	def doScan() : Unit = {
+		logBar()
+		myS4JLogger.info(s"Pretending to scan prqt file=${rsrcPth_lmlExpWebJson}, how bow da?")
+		logBar()
+	}
+
+
+	private def logBar() : Unit = {
+		myS4JLogger.info("================================================================")
+	}
+
+}
